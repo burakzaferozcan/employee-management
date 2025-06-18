cd97c691bec725ec398d63c5ab4b6a15cbb036f8
burakzaferozcan <ozcanburakzafer@gmail.com>
2025-06-18 14:11:25 +0300

    fix: Correct typo in fillable property for Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,7 +10,7 @@ class Employee extends Model
     use HasFactory;
 
     protected $fillable = [
-        "fist_name",
+        "first_name",
         "last_name",
         "address",
         "city_id",
